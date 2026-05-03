1cc7e2744e4c3b9a3fe37029b6c963d8adcc0370
ajbeltranl17-cpu <aj.beltranl17@gmail.com>
2026-05-03 09:59:12 -0600

    Conección con Firebase

---
+++ src/firebase.ts
@@ -0,0 +1,20 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
+
+// Tu configuración web de Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyCnqUTfbtKiQIzxwv9NpcSJlvLpXGm90OI",
+  authDomain: "tuconexiongratis.firebaseapp.com",
+  projectId: "tuconexiongratis",
+  storageBucket: "tuconexiongratis.appspot.com",
+  messagingSenderId: "440980361178",
+  appId: "1:440980361178:web:733159d8332a23c6f8fae3"
+};
+
+// Inicializar Firebase
+const app = initializeApp(firebaseConfig);
+
+// Inicializar y exportar los servicios que usaremos en la app
+export const db = getFirestore(app);
+export const storage = getStorage(app);
